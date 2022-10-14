9c178a549e15d694545c63d382867036aac11cb8
Maksym Lenets <maximlenets@gmail.com>
2022-10-14 16:09:27 +0300

    init commit

---
+++ src/main/java/academy/softserve/library/config/WebConfig.java
@@ -0,0 +1,21 @@
+package academy.softserve.library.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan(basePackages = "academy.softserve.library.controller")
+@EnableWebMvc
+public class WebConfig {
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/test.jsp
@@ -0,0 +1,17 @@
+
+<%--
+  Created by IntelliJ IDEA.
+  User: MY_PC
+  Date: 13.10.2022
+  Time: 15:34
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>HELLO</h1>
+</body>
+</html>
