1ee8491951c5af7e73ccdd43ca2f179e09ae2cfe
Maksym Lenets <maximlenets@gmail.com>
2022-10-20 15:31:51 +0300

    add_book_dto

---
+++ src/main/java/academy/softserve/library/dto/BookDto.java
@@ -0,0 +1,36 @@
+package academy.softserve.library.dto;
+
+import academy.softserve.library.model.Author;
+import academy.softserve.library.model.Book;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookDto {
+
+    private Long id;
+
+    private String title;
+
+    //TODO replace with DTO
+    private Author author;
+
+    private BookDto() {
+    }
+
+    public static BookDto toBookDto(Book book) {
+        BookDto bookDto = new BookDto();
+        bookDto.id = book.getId();
+        bookDto.title = book.getTitle();
+        bookDto.author = book.getAuthor();
+        return bookDto;
+    }
+
+    public Book toBook(Book book) {
+        book.setTitle(this.title);
+        book.setAuthor(this.author);
+        return book;
+    }
+
+}
