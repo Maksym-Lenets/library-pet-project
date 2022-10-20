8c1b12f7a13f3b54ffcdc258f01f5cd3bf29caca
Maksym Lenets <maximlenets@gmail.com>
2022-10-20 15:16:39 +0300

    upd_book_service

--- src/main/java/academy/softserve/library/service/BookService.java
+++ src/main/java/academy/softserve/library/service/BookService.java
@@ -1,4 +1,17 @@
 package academy.softserve.library.service;
 
+import academy.softserve.library.model.Book;
+
+import java.util.List;
+
 public interface BookService {
+    List<Book> getAll();
+
+    Book get(Long id);
+
+    Book save(Book book);
+
+    Book update(Book book);
+
+    boolean remove(Long id);
 }
