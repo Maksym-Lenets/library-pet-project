61c912beb20962130f7196927e611a17105b9385
Serhiy Oliyarnik <sergoliarnik@gmail.com>
2022-10-23 22:38:16 +0300

    add methods to services and daos

--- src/main/java/academy/softserve/library/repository/hibernate/HibernateRequestRepositoryImpl.java
+++ src/main/java/academy/softserve/library/repository/hibernate/HibernateRequestRepositoryImpl.java
@@ -2,16 +2,18 @@ package academy.softserve.library.repository.hibernate;
 
 import academy.softserve.library.model.Request;
 import academy.softserve.library.repository.RequestRepository;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public class HibernateRequestRepositoryImpl implements RequestRepository {
 
-    private SessionFactory sessionFactory;
+    private final SessionFactory sessionFactory;
 
     @Autowired
     public HibernateRequestRepositoryImpl(SessionFactory sessionFactory) {
@@ -20,21 +22,42 @@ public class HibernateRequestRepositoryImpl implements RequestRepository {
 
     @Override
     public List<Request> getAll() {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        return session.createQuery("from Request", Request.class).list();
     }
 
     @Override
     public Request get(Long id) {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        return session.load(Request.class, id);
+    }
+
+    @Override
+    public List<Request> get(LocalDate from, LocalDate to) {
+        Session session = sessionFactory.getCurrentSession();
+        String hpl = "FROM Request WHERE getBookDate BETWEEN :from AND :to";
+        List<Request> list = session.createQuery(hpl)
+                .setParameter("from", from)
+                .setParameter("to", to)
+                .list();
+        return list;
     }
 
     @Override
     public Request saveOrUpdate(Request element) {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        session.saveOrUpdate(element);
+        return element;
     }
 
     @Override
     public boolean remove(Long id) {
-        return false;
+        Session session = sessionFactory.getCurrentSession();
+        Request request = get(id);
+        if (request == null)return false;
+        session.remove(request);
+        return true;
     }
+
+
 }
--- src/main/java/academy/softserve/library/service/impl/AuthorServiceImpl.java
+++ src/main/java/academy/softserve/library/service/impl/AuthorServiceImpl.java
@@ -1,9 +1,13 @@
 package academy.softserve.library.service.impl;
 
+import academy.softserve.library.model.Author;
 import academy.softserve.library.repository.AuthorRepository;
 import academy.softserve.library.service.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class AuthorServiceImpl implements AuthorService {
@@ -13,6 +17,30 @@ public class AuthorServiceImpl implements AuthorService {
     @Autowired
     public AuthorServiceImpl(AuthorRepository authorRepository) {
         this.authorRepository = authorRepository;
+    }
+
+    @Transactional
+    @Override
+    public List<Author> getAll(){
+        return authorRepository.getAll();
+    }
+
+    @Transactional
+    @Override
+    public Author get(Long id){
+        return authorRepository.get(id);
+    }
+
+    @Transactional
+    @Override
+    public Author saveOrUpdate(Author author) {
+        return authorRepository.saveOrUpdate(author);
+    }
+
+    @Transactional
+    @Override
+    public boolean delete(Long id) {
+        return authorRepository.remove(id);
     }
 
 }
