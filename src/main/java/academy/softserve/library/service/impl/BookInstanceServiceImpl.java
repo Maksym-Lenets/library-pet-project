516a63e8e2415f088ef1ae7f21572c876767e819
Maksym Lenets <maximlenets@gmail.com>
2022-10-25 10:28:13 +0300

    upd_book_inst_service

--- src/main/java/academy/softserve/library/service/impl/BookInstanceServiceImpl.java
+++ src/main/java/academy/softserve/library/service/impl/BookInstanceServiceImpl.java
@@ -1,8 +1,11 @@
 package academy.softserve.library.service.impl;
 
+import academy.softserve.library.model.BookInstance;
 import academy.softserve.library.repository.BookInstanceRepository;
 import academy.softserve.library.service.BookInstanceService;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 public class BookInstanceServiceImpl implements BookInstanceService {
     private BookInstanceRepository bookInstanceRepository;
@@ -13,4 +16,28 @@ public class BookInstanceServiceImpl implements BookInstanceService {
     }
 
 
+    @Override
+    public List<BookInstance> getAll() {
+        return bookInstanceRepository.getAll();
+    }
+
+    @Override
+    public BookInstance get(Long id) {
+        return bookInstanceRepository.get(id);
+    }
+
+    @Override
+    public BookInstance save(BookInstance bookInstance) {
+        return bookInstanceRepository.saveOrUpdate(bookInstance);
+    }
+
+    @Override
+    public BookInstance update(BookInstance bookInstance) {
+        return bookInstanceRepository.saveOrUpdate(bookInstance);
+    }
+
+    @Override
+    public boolean remove(Long id) {
+        return bookInstanceRepository.remove(id);
+    }
 }
