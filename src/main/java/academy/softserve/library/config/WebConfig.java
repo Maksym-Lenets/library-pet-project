35d3a4732d5e5fa91dc4c21c631da376b339789b
Maksym Lenets <maximlenets@gmail.com>
2022-10-28 00:53:01 +0300

    add_resource_handler

--- src/main/java/academy/softserve/library/config/WebConfig.java
+++ src/main/java/academy/softserve/library/config/WebConfig.java
@@ -23,7 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("/webjars/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/");
+        registry.addResourceHandler("/resources/**" ).addResourceLocations("/resources/" );
     }
 }
